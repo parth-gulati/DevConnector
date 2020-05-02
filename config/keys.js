0a58e3d621d970d300f7c8755ef2fcb90aae366d
Parth Gulati <gulati.parth98@gmail.com>
2020-05-02 21:04:55 +0530

    Initial express server with routes files

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://root:462606@devconnecter-xrl6f.mongodb.net/test?retryWrites=true&w=majority",
+};
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+const app = express();
+
+//db Config
+
+const db = require("./config/keys").mongoURI;
+
+//Connect to MongoDB
+
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/profile", profile);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server Running on Port ${port} `));
