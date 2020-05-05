5a8becc686b6ae2e01c0948f377f7dc6c19dedc1
Parth Gulati <gulati.parth98@gmail.com>
2020-05-05 21:50:28 +0530

    Added JWT Authorisation and Login registration backend

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
     "mongodb+srv://root:462606@devconnecter-xrl6f.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret",
 };
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+//Body Parser middleware
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //db Config
 
@@ -22,6 +29,13 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
+//Passport middleware
+app.use(passport.initialize());
+
+//Passport Config
+require("./config/passport")(passport);
+
+//Use Routes
 app.use("/api/users", users);
 app.use("/api/posts", posts);
 app.use("/api/profile", profile);
